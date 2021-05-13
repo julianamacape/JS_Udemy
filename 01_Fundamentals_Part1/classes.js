7bd6f864c49e2a7203da72bba9e52859340a8d6c
Juliana Pereira <jotace1234@hotmail.com>
2021-05-13 10:37:10 -0300

    update

---
+++ 01_Fundamentals_Part1/classes.js
@@ -0,0 +1,5 @@
+const firstName = 'Juliana';
+const age = 34;
+const job = 'web developer and programmer';
+const juliana = "Hi! I'm " + firstName + ": a " + age + "-year-old " + job + ". Nice to meet you all.";
+console.log(juliana)
