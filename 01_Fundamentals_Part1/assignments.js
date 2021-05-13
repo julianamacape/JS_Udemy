e7a909c35ae8d468916fc88de38eb30b99cd6ced
Juliana Pereira <jotace1234@hotmail.com>
2021-05-13 10:17:47 -0300

    update

---
+++ 01_Fundamentals_Part1/assignments.js
@@ -0,0 +1,37 @@
+//LECTURE: Values and Variables
+
+const country = "Brazil";
+const continent = "South America";
+let population = 200000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// LECTURE: Data Types
+
+const isIsland = false;
+let language;
+
+console.log(isIsland);
+console.log(language);
+
+//LECTURE: let, const and var
+
+language = "portuguese";
+
+// LECTURE: Basic Operators
+
+//1 & 2
+console.log((population / 2) + 1);
+
+//3.
+const finlandPop = 6000000
+console.log(population > finlandPop);
+
+//4.
+let averageCountryPop = 33000000;
+console.log(population < averageCountryPop);
+
+//5.
+let description = 'Brazil is is South America, and its 200 million people speak Portuguese.'
---
+++ 01_Fundamentals_Part1/coding_challenge01.js
@@ -0,0 +1,22 @@
+//Test data #1
+/*
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJohn = 1.95;
+let markBMI = massMark / (heightMark ** 2);
+let johnBMI = massJohn / (heightJohn ** 2);
+let markHigherBMI = markBMI > johnBMI
+
+console.log(markHigherBMI)
+*/
+
+//Test data #2
+let massMark = 95;
+let heightMark = 1.88;
+let massJohn = 85;
+let heightJohn = 1.76;
+let markBMI = massMark / (heightMark ** 2);
+let johnBMI = massJohn / (heightJohn ** 2);
+let markHigherBMI = markBMI > johnBMI
+console.log(markHigherBMI)
