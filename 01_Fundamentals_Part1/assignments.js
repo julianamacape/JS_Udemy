c409197796c4e3d2de99ec1b04d8d72c8e4b0f00
Juliana Pereira <jotace1234@hotmail.com>
2021-05-18 10:19:26 -0300

    update

--- 01_Fundamentals_Part1/assignments.js
+++ 01_Fundamentals_Part1/assignments.js
@@ -2,7 +2,7 @@
 
 const country = "Brazil";
 const continent = "South America";
-const population = 200000000;
+const population = 200;
 
 console.log(country);
 console.log(continent);
@@ -26,17 +26,51 @@ language = "portuguese";
 console.log((population / 2) + 1);
 
 //3.
-const finlandPop = 6000000
+const finlandPop = 6
 console.log(population > finlandPop);
 
 //4.
-const averageCountryPop = 33000000;
+const averageCountryPop = 33;
 console.log(population < averageCountryPop);
 
 //5.
 const description = 'Brazil is is South America, and its 200 million people speak Portuguese.'
 console.log(description);
 
-//6.
+// LECTURE: Strings and Template Literals
 const description2 = `${country} is in ${continent} and its ${population} million people speak ${language}.`
 console.log(description2);
+
+// LECTURE: Taking Decisions: if / else Statements
+let diffPopulation = 33 - population;
+if (population > 33) {
+    console.log(`${country}'s population is above average.`)
+} else {
+    console.log(`${country}'s population is ${diffPopulation} million below everage.`)
+}
+
+// LECTURE: Type Conversion and Coercion
+console.log('9' - '5'); // o sinal de subtração vai COAGIR as strings a virarem números então = 4
+console.log('19' - '13' + '17'); // o sinal de subtração vai COAGIR as strings a virarem números então = 6 e a adição vai virar string então = 617
+console.log('19' - '13' + 17); // o sinal de subtração vai COAGIR as strings a virarem números então = 6 e a adição vai somar normal então = 23
+console.log('123' < 57); // o sinal de menor-que vai COAGIR a string a virar número então 123 < 57 = falso
+console.log(5 + 6 + '4' + 9 - 4 - 2); /* os primeiros dois numeros somam-se normalmente, ai em seguida viram string então = '114', ai o 9 vira string também então = '1149' e o sinal de subtração faz tudo virar número então = 1143
+*/
+
+/* LECTURE: Equality Operators: == vs. ===
+const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+if (numNeighbours === 1) {
+    console.log("Only 1 border!");
+} else if (numNeighbours > 1) {
+    console.log("More than 1 border");
+} else {
+    console.log("No borders");
+}
+*/
+
+// LECTURE: Logical Operators
+if (language === "english" && population < 50 && isIsland === false) {
+    console.log(`Sarah, you should definitely live in ${country}.`)
+} else {
+    console.log(`I'm sorry, Sarah, but ${country} isn't the right place for you.`)
+}
--- 01_Fundamentals_Part1/coding_challenge03.js
+++ 01_Fundamentals_Part1/coding_challenge03.js
@@ -1,6 +1,6 @@
+/* Test data
 const avgDolphins = (96 + 108 + 89) / 3;
 const avgKoalas = (88 + 91 + 110) / 3;
-console.log(avgKoalas)
 
 if (avgDolphins > avgKoalas) {
     console.log("The Dolphins are the winners!");
@@ -9,5 +9,38 @@ if (avgDolphins > avgKoalas) {
 } else if (avgDolphins === avgKoalas) { //apesar de não ser necessário declara isso pq ou se nenhum time ganhou/perdeu então foi empate, mas td bem
     console.log("There was a draw! Both teams are winners!")
 }
+*/
 
-//Falta Bonus 1 and Bonus 2
+/* Bonus 1
+const avgDolphins = (97 + 112 + 101) / 3;
+const avgKoalas = (109 + 95 + 123) / 3;
+
+if (avgDolphins > avgKoalas && avgDolphins >= 100) {
+    console.log("The Dolphins are the ultimate winners!");
+} else if (avgDolphins > avgKoalas && avgDolphins < 100) {
+    console.log("The Dolphins beat the Koalas, but didn't score the mark of 100 points.")
+} else if (avgKoalas > avgDolphins && avgKoalas >= 100) {
+    console.log("The Koalas are the ultimate winners!");
+} else if (avgKoalas > avgDolphins && avgKoalas < 100) {
+    console.log("The Koalas beat the Dolphins, but didn't score the mark of 100 points.")
+} else {
+    console.log("There was a draw! Both teams are winners!")
+}
+*/
+
+/* Bonus 2
+const avgDolphins = (97 + 112 + 101) / 3;
+const avgKoalas = (109 + 95 + 106) / 3;
+
+if (avgDolphins > avgKoalas && avgDolphins >= 100) {
+    console.log("The Dolphins are the ultimate winners!");
+} else if (avgDolphins > avgKoalas && avgDolphins < 100) {
+    console.log("The Dolphins beat the Koalas, but didn't score the mark of 100 points.")
+} else if (avgKoalas > avgDolphins && avgKoalas >= 100) {
+    console.log("The Koalas are the ultimate winners!");
+} else if (avgKoalas > avgDolphins && avgKoalas < 100) {
+    console.log("The Koalas beat the Dolphins, but didn't score the mark of 100 points.")
+} else if (avgDolphins === avgKoalas && avgDolphins >= 100 && avgKoalas >= 100) {
+    console.log("There was a draw! Both teams are winners!")
+}
+*/
