621cc3b9169e391cfdeb7f7310c1e69b0f6cd984
Juliana Pereira <jotace1234@hotmail.com>
2021-05-27 10:33:27 -0300

    update

--- 02_Fundamentals_Part2/assignments.js
+++ 02_Fundamentals_Part2/assignments.js
@@ -106,3 +106,9 @@ function describePopulation(country, population) {
 console.log(describePopulation('Canada', 37.8));
 console.log(describePopulation('Portugal', 11));
 console.log(describePopulation('the United States', 328));
+
+// LECTURE: Introduction to Arrays
+const allPopulations = [200, 328, 37.8, 11];
+console.log(allPopulations.length === 4);
+const allPercentages = [percentageOfWorld1(allPopulations[0]), percentageOfWorld1(allPopulations[1]), percentageOfWorld1(allPopulations[2]), percentageOfWorld1(allPopulations[3])];
+console.log(allPercentages);
