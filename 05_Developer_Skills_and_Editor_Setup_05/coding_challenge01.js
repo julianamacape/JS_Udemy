71fe06d2c5a611ce03459a9c80e6c5cb0e01d83b
Juliana Pereira <jotace1234@hotmail.com>
2021-06-15 08:52:55 -0300

    update

--- 05_Developer_Skills_and_Editor_Setup_05/coding_challenge01.js
+++ 05_Developer_Skills_and_Editor_Setup_05/coding_challenge01.js
@@ -1,12 +1,19 @@
 "use strict";
 const printForecast = function (arr) {
+  let strSum = "";
   for (let i = 0; i < arr.length; i++) {
-    console.log(`...${arr[i]}C in ${i + 1} days`);
+    strSum += `${arr[i]}C in ${i + 1} days 🥵🥶😄`;
   }
+  return `🧭 ${strSum}`;
 };
 
 const testData1 = [17, 21, 23];
 const testData2 = [12, 5, -5, 0, 4];
 
-printForecast(testData1);
-printForecast(testData2);
+console.log(printForecast(testData1));
+console.log(printForecast(testData2));
+
+/*
+Da mesma forma que usamos a variável Sum para somar valores numérios de uma array, também podemos somar as strings de uma array.
+Ao invés de Sum = 0, pq obviamente não é um número, Sum = ''; que é o que equivale a uma string vazia
+*/
