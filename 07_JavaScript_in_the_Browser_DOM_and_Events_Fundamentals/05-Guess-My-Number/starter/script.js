b51cae35edb418df8a970897a2bfdffb08b34b05
Juliana Pereira <jotace1234@hotmail.com>
2021-06-17 14:35:15 -0300

    update

--- 07_JavaScript_in_the_Browser_DOM_and_Events_Fundamentals/05-Guess-My-Number/starter/script.js
+++ 07_JavaScript_in_the_Browser_DOM_and_Events_Fundamentals/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.check').addEventListener('click', function () {
+  let userGuess = document.querySelector('.guess').value;
+  if (userGuess.length == 0) {
+    //if (!userGuess)  significa a mesma coisa, ou seja, chega no mesmo resultado
+    document.querySelector('.message').textContent = '🚫 No number!';
+  } else if (userGuess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too high. Try again.';
+  } else if (userGuess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too low. Try again.';
+  } else if (userGuess == secretNumber) {
+    document.querySelector('.message').textContent = '🏆 You won the game!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+  }
+});
