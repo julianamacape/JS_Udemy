a5f28fefb99ceb58093e326e2fff69552a698de7
Juliana Pereira <jotace1234@hotmail.com>
2021-06-18 08:46:09 -0300

    update

--- 07_JavaScript_in_the_Browser_DOM_and_Events_Fundamentals/05-Guess-My-Number/starter/script.js
+++ 07_JavaScript_in_the_Browser_DOM_and_Events_Fundamentals/05-Guess-My-Number/starter/script.js
@@ -1,6 +1,12 @@
 'use strict';
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let curScore = document.querySelector('.score');
+let curScoreNbr = 20;
+
+let curHighscore = document.querySelector('.highscore');
+
 document.querySelector('.check').addEventListener('click', function () {
   let userGuess = document.querySelector('.guess').value;
   if (userGuess.length == 0) {
@@ -8,11 +14,43 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🚫 No number!';
   } else if (userGuess > secretNumber) {
     document.querySelector('.message').textContent = '📈 Too high. Try again.';
+    curScoreNbr--;
+    curScore.textContent = curScoreNbr;
+    if (curScoreNbr == 0) {
+      document.querySelector('.message').textContent =
+        '😩 GAME OVER! You lose. Press AGAIN';
+      document.querySelector('body').style.backgroundColor = 'red';
+      document.querySelector('.number').textContent = secretNumber;
+    }
+    if (curScoreNbr < 0) {
+      curScore.textContent = 'X';
+      document.querySelector('.message').textContent =
+        '😩 GAME OVER! You lose. Press AGAIN';
+    }
+    if (curScoreNbr < 0) curScore.textContent = 'X';
   } else if (userGuess < secretNumber) {
     document.querySelector('.message').textContent = '📉 Too low. Try again.';
+    curScoreNbr--;
+    curScore.textContent = curScoreNbr;
+    if (curScoreNbr == 0) {
+      document.querySelector('.message').textContent =
+        '😩 GAME OVER! You lose. Press AGAIN';
+      document.querySelector('body').style.backgroundColor = 'red';
+      document.querySelector('.number').textContent = secretNumber;
+    }
+    if (curScoreNbr < 0) {
+      curScore.textContent = 'X';
+      document.querySelector('.message').textContent =
+        '😩 GAME OVER! You lose. Press AGAIN';
+    }
   } else if (userGuess == secretNumber) {
     document.querySelector('.message').textContent = '🏆 You won the game!';
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
+    curHighscore.textContent = curScore.textContent;
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('body').style.backgroundColor = '#222';
 });
