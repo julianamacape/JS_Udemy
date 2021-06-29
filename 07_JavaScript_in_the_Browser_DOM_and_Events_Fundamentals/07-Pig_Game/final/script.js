730e19316e7d341265a5e3a97c4868ae3a3b9009
Juliana Pereira <jotace1234@hotmail.com>
2021-06-28 22:10:14 -0300

    update

--- 07_JavaScript_in_the_Browser_DOM_and_Events_Fundamentals/07-Pig_Game/final/script.js
+++ 07_JavaScript_in_the_Browser_DOM_and_Events_Fundamentals/07-Pig_Game/final/script.js
@@ -1,5 +1,47 @@
 'use strict';
 
+// Naming the players:
+////// 1) using the hardcoding method (to be akcnowledged, not to be used)
+/*
+const playerOne = prompt('Qual o nome do Jogador 01?');
+document.querySelector('#name--0').textContent = playerOne;
+const playerTwo = prompt('Qual o nome do Jogador 02?');
+document.querySelector('#name--1').textContent = playerTwo;
+*/
+
+////// 2) using a function
+let listPlayers = [];
+
+const playersNames = function (nbrPlayers) {
+  for (let i = 0; i < nbrPlayers; i++) {
+    listPlayers[i] = prompt(`What's the name of Player ${i + 1}?`);
+  }
+  return listPlayers;
+};
+
+playersNames(2); //In this particular game, it'll always be 2 players
+
+/* FIND BELOW THE FUNCTION TO BE USED IN CASE YOU WANT THE USER TO TELL YOU HOW MANY PLAYERS ARE GOING TO PLAY PROVIDED THAT, OF COURSE, YOUR GAME ALLOWS 2+ PLAYERS:
+
+const numberPlayers = function () {
+  const howManyPlayers = Number(prompt('How many players?'));
+  howManyPlayers < 2
+    ? alert('The number of players must be 2+. Please refresh the page.')
+    : playersNames(howManyPlayers);
+};
+
+numberPlayers();
+*/
+
+let playerName = document.querySelectorAll('.name');
+
+for (let i = 0; i < listPlayers.length; i++) {
+  playerName[i].textContent = listPlayers[i];
+}
+
+
+
+
 // Selecting elements
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
@@ -14,6 +56,7 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 let scores, currentScore, activePlayer, playing;
+
 
 // Starting conditions
 const init = function () {
@@ -57,9 +100,8 @@ btnRoll.addEventListener('click', function () {
     if (dice !== 1) {
       // Add dice to current score
       currentScore += dice;
-      document.getElementById(
-        `current--${activePlayer}`
-      ).textContent = currentScore;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
     } else {
       // Switch to next player
       switchPlayer();
