abc70f00b6bab2c9f983a7eee8bfcf1d05a03d05
Juliana Pereira <jotace1234@hotmail.com>
2021-07-05 10:36:09 -0300

    update

--- 07_JavaScript_in_the_Browser_DOM_and_Events_Fundamentals/07-Pig_Game/starter/script.js
+++ 07_JavaScript_in_the_Browser_DOM_and_Events_Fundamentals/07-Pig_Game/starter/script.js
@@ -38,3 +38,45 @@ let playerName = document.querySelectorAll('.name');
 for (let i = 0; i < listPlayers.length; i++) {
   playerName[i].textContent = listPlayers[i];
 }
+
+//Selecting elements
+const score0El = document.querySelector('#score--0');
+const current0El = document.querySelector('#current--0');
+const score1El = document.querySelector('#score--1');
+const current1El = document.querySelector('#current--1');
+const currentScoreBoth = document.querySelector('.current-score');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//Initial conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //1. Generate a random number (dice roll)
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  //2. Display the dice image according to the number outcome
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  //3. Check if number 1 was outcome; if TRUE, switch to the other player
+  if (dice !== 1) {
+    //Add the value of dice to the current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    //switch to the other player
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    //In other words: is the activePlayer "0"? Yes? So Switch to "1". No? So switch do "0"
+    currentScore = 0;
+    currentScoreBoth.textContent = 0;
+  }
+});
