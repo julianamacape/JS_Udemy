4c041831e6e061d38021ac7768b8f10f5e21350f
Juliana Pereira <jotace1234@hotmail.com>
2021-07-20 11:04:43 -0300

    update

---
+++ 08_How JavaScript Works Behind the Scenes/08-How-JS-works/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const juliana = {
+    firstName: 'Juliana',
+    year: 1987,
+    
+    calcAge: function() {
+        console.log(2021 - this.year);
+
+        const isMillenial = function() {
+            console.log(this);
+            console.log(this.year >= 1981 && this.year <= 1996);
+        };
+        isMillenial();
+    },
+    
+    greet() => {
+        console.log(this);
+        console.log(`Hey ${this.firstName}`);
+    },
+};
+
+juliana.greet();
