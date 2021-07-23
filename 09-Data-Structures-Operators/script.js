c0f3672b090f5e4c492ce2261a4670c8b22b3601
Juliana Pereira <jotace1234@hotmail.com>
2021-07-23 08:43:30 -0300

    update

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,5 +1,26 @@
 "use strict";
 
-const nested = [1, 2, [3, 4, 5]];
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
 
-console.log(nested[2][2]);
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+console.log(restaurant.openingHours.thu.open);
