4e43a54da6507d2715eebe9fa7b7e5953ae6c6e9
Juliana Pereira <jotace1234@hotmail.com>
2021-07-29 11:30:16 -0300

    update

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -33,6 +33,19 @@ const game = {
       "Sancho",
       "Gotze",
     ],
+    [
+      "Lukas Hradecky",
+      "Panagiotis Retsos",
+      "Jonathan Tah",
+      "Mitchel Bakker",
+      "Odilon Kossounou",
+      "Edmond Tapsoba",
+      "Wendell",
+      "Daley Sinkgraven",
+      "Mitchell Weiser",
+      "Timothy Fosu-Mensah",
+      "Jeremie Frimpong",
+    ],
   ],
   score: "4:0",
   scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
@@ -43,3 +56,16 @@ const game = {
     team2: 6.5,
   },
 };
+
+//1. Create one player array for each team (variables 'players1' and 'players2')
+// I used 'destructuring' here.
+const [players1, players2] = game.players;
+
+console.log(players1, players2);
+
+/* 
+2. The first player in any player array is the goal keeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+*/
+//I used 'destructuring' and the 'rest' pattern here
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
