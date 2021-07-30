cc6f38aa10bbbc7a983db93f9245714b423f2473
Juliana Pereira <jotace1234@hotmail.com>
2021-07-30 15:56:25 -0300

    update

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -60,6 +60,7 @@ const [gk, ...fieldPlayers] = players1;
 //3. Create an array 'allPlayers' containing all players of both teams (22 players)
 //I used the "spread" operator here
 const allPlayers = [...game.players[0], ...game.players[1]];
+// or const allPlayers = [...players1, ...players2];
 //console.log(allPlayers);
 
 //4. During the game, Bayern Munich (team1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
