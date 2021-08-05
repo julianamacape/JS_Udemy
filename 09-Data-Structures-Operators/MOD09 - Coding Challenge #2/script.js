ed34dc977cc1db1ab663102420595ce73a9321e5
Juliana Pereira <jotace1234@hotmail.com>
2021-08-05 12:18:26 -0300

    update

--- 09-Data-Structures-Operators/MOD09 - Coding Challenge #2/script.js
+++ 09-Data-Structures-Operators/MOD09 - Coding Challenge #2/script.js
@@ -1,7 +1,12 @@
 "use strict";
 
 //Data Structures, Modern Operators and Strings
-//Coding challenge #1 - Betting app
+//Coding challenge #2 - Betting app
+const odds = {
+  team1: 1.33,
+  x: 3.25,
+  team2: 6.5,
+};
 
 const game = {
   team1: "Bayern Munich",
@@ -34,56 +39,45 @@ const game = {
       "Gotze",
     ],
   ],
+  odds,
   score: "4:0",
   scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
   date: "Nov 9th, 2037",
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
 };
 
-//1. Create one player array for each team (variables 'players1' and 'players2')
-// I used 'destructuring' here.
-const [players1, players2] = game.players;
+//1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+//I used destructuring and for-op to loop over the game.scored array
+// for (const player of game.scored) console.log(player); -- this is just to see what it does
+// for (const player of game.scored.entries()) console.log(player); -- this is just to see what it does
+for (const [index, player] of game.scored.entries()) {
+  console.log(`Goal ${index + 1} by ${player}`);
+}
 
-//console.log(players1, players2);
+// 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
 
-/* 
-2. The first player in any player array is the goal keeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+/* This was my solution (kinda long)
+const totalOdds = Object.values(odds);
+//console.log(totalOdds);
+const aveOdd = function (...oddsValues) {
+  let oddSum = 0;
+  let oddAve = 0;
+  for (let i = 0; i < oddsValues.length; i++) {
+    oddSum += oddsValues[i];
+  }
+  oddAve = oddSum / oddsValues.length;
+  console.log(oddAve);
+};
+
+aveOdd(...totalOdds);
 */
-//I used arrays 'destructuring' and the 'rest' pattern here
-const [gk, ...fieldPlayers] = players1;
-//console.log(gk, fieldPlayers);
 
-//3. Create an array 'allPlayers' containing all players of both teams (22 players)
-//I used the "spread" operator here
-const allPlayers = [...game.players[0], ...game.players[1]];
-// or const allPlayers = [...players1, ...players2];
-//console.log(allPlayers);
-
-//4. During the game, Bayern Munich (team1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
-//I used the "spread" operator
-const playersFinal1 = [...players1, "Thiago", "Coutinho", "Perisic"];
-//console.log(playersFinal1);
-
-//5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
-//I used objects "destructuring" and variables names alteration here
-const { team1, x: draw, team2 } = game.odds;
-//console.log(team1, draw, team2);
-
-//6. Write a function ('printGoals') that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
-//I used the 'rest' patterns with functions and I also used o "spread" operator
-const printGoals = function (...playerName) {
-  console.log(playerName);
-  let nbrGoals = playerName.length;
-  console.log(`Total of ${nbrGoals} goals.`);
-};
-printGoals("Davies");
-printGoals("Davies", "Muller");
-printGoals("Davies", "Muller", "Lewandowski");
-printGoals(...game.scored);
-
-//7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, without using an if/else statement or the ternary operator.
-console.log(`Team 1: odds ${team1}` || `Team 2: odds ${team2}`);
+// I enhanced my solution after watching the teacher's solution
+let aveOdd = 0;
+let sumOdd = 0;
+const totalOdds = Object.values(odds);
+for (const odd of totalOdds) {
+  sumOdd += odd;
+}
+//console.log(sumOdd);
+aveOdd = sumOdd / totalOdds.length;
+console.log(aveOdd);
