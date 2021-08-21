16cfd62707b0df9150baf250232bf3972d00b93e
Juliana Pereira <jotace1234@hotmail.com>
2021-08-21 10:18:47 -0300

    update

---
+++ 09-Data-Structures-Operators/MOD09 - Coding Challenge #3/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//Data Structures, Modern Operators and Strings
+//Coding challenge #3 - Betting app
+
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
