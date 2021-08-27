290ac4d1f1be5989324bee0ff6629249512cc6aa
Juliana Pereira <jotace1234@hotmail.com>
2021-08-27 14:15:16 -0300

    update

--- 09-Data-Structures-Operators/MOD09 - Coding Challenge #4/script.js
+++ 09-Data-Structures-Operators/MOD09 - Coding Challenge #4/script.js
@@ -2,3 +2,6 @@
 
 //Data Structures, Modern Operators and Strings
 //Coding challenge #4
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
