88b4413e7e0b59dbfc0a73dfaa9c660092bc929b
Juliana Pereira <jotace1234@hotmail.com>
2021-09-02 10:57:27 -0300

    update

--- 10-A-Closer-Look-At-Functions/starter/script.js
+++ 10-A-Closer-Look-At-Functions/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const book = {
+  booker(flightNum, name) {
+    console.log(`${name} booked a seat on flight ${flightNum}`);
+  },
+};
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function() {}
+  book,
+};
+
+lufthansa.book.booker(239, 'Jonas Schmedtmann');
+//lufthansa.book(635, 'John Smith');
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
