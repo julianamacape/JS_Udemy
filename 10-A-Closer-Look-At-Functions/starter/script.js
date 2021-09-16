b91f1c7f6ccba97dda5a6723ec3f3e516afc01da
Juliana Pereira <jotace1234@hotmail.com>
2021-09-16 12:10:04 -0300

    update

--- 10-A-Closer-Look-At-Functions/starter/script.js
+++ 10-A-Closer-Look-At-Functions/starter/script.js
@@ -1,13 +1,25 @@
 'use strict';
-
-const myObject = {
-  name: 'Ju',
-  age: 34,
-  greet: function () {
-    console.log(this);
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function() {}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
   },
 };
 
-myObject.location = 'Quebec city';
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
 
-myObject.greet();
+  this.planes++;
+  console.log(this.planes);
+};
+lufthansa.buyPlane();
+
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane);
