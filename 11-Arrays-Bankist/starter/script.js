f8e2de27ff6e60247f56254c2dd4bb865dcf9861
Juliana Pereira <jotace1234@hotmail.com>
2021-09-27 12:23:19 -0300

    update

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,7 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+const arr = [1, 2, 3, 4, 5];
+console.log(arr.splice(2));
+console.log(arr);
