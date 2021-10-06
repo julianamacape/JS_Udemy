4a9c6bcf478f1dceaeaaa06407c649c3fabae38a
Juliana Pereira <jotace1234@hotmail.com>
2021-10-06 11:32:33 -0300

    update

--- 11-Arrays-Bankist/MOD11 - coding challenge #1/script.js
+++ 11-Arrays-Bankist/MOD11 - coding challenge #1/script.js
@@ -1,4 +1,28 @@
 "use strict";
 
 //Working With Arrays
-//Coding challenge #1 -
+//Coding challenge #1 - A Study About Dogs
+
+//Step 0: Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things
+const checkDogs = function (dogsJulia, dogsKate) {
+  //Step 1: Julia found out that the owners of the first and the last two dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  //Step 2: Create an array with both Julia's (corrected) and Kate's data
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  //Step 3:For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+  dogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+    }
+  });
+};
+//Step 4:Run the function for both test datasets
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+//Test data Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+//Test Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
