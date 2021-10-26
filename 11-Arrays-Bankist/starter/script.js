de475e78b69e51ca39e84e123d999d3ca61f9571
Juliana Pereira <jotace1234@hotmail.com>
2021-10-26 08:36:18 -0300

    update

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -70,7 +70,7 @@ const displayMovements = function (movements) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}</div>
+        <div class="movements__value">${mov}£</div>
       </div>`;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -85,6 +85,26 @@ const calcDisplayBalance = function (movements) {
 };
 
 calcDisplayBalance(account2.movements);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}£`;
+
+  const outcomes = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(outcomes)}£`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}£`;
+};
+
+calcDisplaySummary(account2.movements);
 
 const createUsername = function (allAccounts) {
   allAccounts.forEach(function (account) {
