bd0897eb56b020c7fe35f57c1a6d6539f49ee66c
Juliana Pereira <jotace1234@hotmail.com>
2021-10-27 12:32:50 -0300

    update

--- 11-Arrays-Bankist/MOD11 - coding challenge #3/script.js
+++ 11-Arrays-Bankist/MOD11 - coding challenge #3/script.js
@@ -1,20 +1,27 @@
 "use strict";
 
 //Working With Arrays
-//Coding challenge #2 - A Study About Dogs' Ages
+//Coding challenge #3 - A Study About Dogs' Ages v2.0
 
-//Task #0 - Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages')
+//Task #1 - Rewrite the calcAverageHumanAge() function as an arrow function and using chaining
+/*
 const calcAverageHumanAge = function (ages) {
-  //Task #1 - Calculate the dog age in human years
   const humanAges = ages.map((age) => (age <= 2 ? 2 * age : 16 + age * 4));
-  //Task #2 - Exclude all dogs that are less than 18 human years old
+
   const adultDogs = humanAges.filter((age2) => age2 >= 18);
-  //Task #3 - Calculate the average human age of all adult dogs
+
   const averageAdults =
     adultDogs.reduce((acc, age3) => acc + age3, 0) / adultDogs.length;
   return averageAdults;
 };
+*/
 
-//Task #4 - Run the function for both test data sets
+const calcAverageHumanAge = (ages) =>
+  ages
+    .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter((age) => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+//Task #2 - Run the function for both test data sets
 console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
 console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
